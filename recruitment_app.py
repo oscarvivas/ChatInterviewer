607ce789aa2b6c06980eb3ba33c64c1fa792ba0c
Oscar Vivas <Oscar.Vivas@endava.com>
2024-04-29 17:05:18 -0500

    main

--- recruitment_app.py
+++ recruitment_app.py
@@ -3,38 +3,37 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import time
+from dotenv import load_dotenv
+from openai import AzureOpenAI
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Configure the client for Azure
+client = AzureOpenAI (
+    api_key="f78763df753c4c469a354c3a39ef6d14",
+    azure_endpoint="https://aihackathoneastcan.openai.azure.com",
+    api_version="2023-07-01-preview" # Ensure you use the correct API version
+)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def ask_question (question):
+    try:
+        # Make an API call to the ChatCompletion model
+        response = client.chat.completions.create(
+            model="gpt-35-turbo-0613", # Ensure the engine name is correct for your setup 
+            messages=[
+                {"role": "system", "content": "Asistent is a large language model trained by openAI."}, 
+                {"role": "user", "content": question }
+            ]
+        )    
+        # Extract the message content from the response
+        answer = response.choices[0].message.content
+        return answer
+    except Exception as e:
+        return str(e) # Return the exception as a string for debugging
+    
+# Example usage of the function
+if __name__ == "__main__":
+    question = "hola"
+    answer = ask_question (question)
+    print("Question:", question)
+    print("Answer:", answer)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
